0f381ebbb48c537187d18520ec2d2a3e30e41966
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-11-03 12:03:08 +0100

    Tests

--- backend/src/main/java/de/marcv42/backend/security/SecurityConfig.java
+++ backend/src/main/java/de/marcv42/backend/security/SecurityConfig.java
@@ -3,12 +3,13 @@ package de.marcv42.backend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
--- backend/src/main/java/de/marcv42/backend/user/UserController.java
+++ backend/src/main/java/de/marcv42/backend/user/UserController.java
@@ -1,11 +1,12 @@
 package de.marcv42.backend.user;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 
@@ -18,7 +19,7 @@ public class UserController
     public String getUser()
     {
         var result = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println(result);
+
         return result.getName();
     }
 
